8a7b18fb62f0ac12d5ca7480ba5d4d0839f83c0e
YeoEunJi <yenje7@naver.com>
2020-04-20 22:30:49 +0900

    first commit

---
+++ codingtest/src/baekjoon/b_10828.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class b_10828 {
+
+	public static void main(String[] args) {
+		
+		Scanner s= new Scanner(System.in);
+		
+		int N= s.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			String order=s.next();
+			
+		}
+	}
+}
