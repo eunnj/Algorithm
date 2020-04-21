792bfacb95dbc62fe3e1f6b7eab999ca7fa8f27e
YeoEunJi <yenje7@naver.com>
2020-04-21 12:35:56 +0900

    second commit 수정필요

--- codingtest/src/baekjoon/b_10828.java
+++ codingtest/src/baekjoon/b_10828.java
@@ -1,18 +1,46 @@
 package baekjoon;
 
 import java.util.Scanner;
+import java.util.Stack;
+
 
 public class b_10828 {
-
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 		Scanner s= new Scanner(System.in);
 		
 		int N= s.nextInt();
 		
+		Stack<Integer> stack = new Stack<Integer>();
+		
 		for(int i=0;i<N;i++) {
 			String order=s.next();
 			
+			switch(order) {
+			case "push":
+				int x = s.nextInt();
+				 stack.push(x);
+				break;
+			case "pop":
+				if(stack.isEmpty()) System.out.print(-1);
+				else  System.out.print(stack.pop());
+				break;
+			case "size":
+				 System.out.print(stack.size());
+				break;
+			case "top":
+				if(stack.isEmpty()) System.out.print(-1);
+				else  System.out.print(stack.peek());
+				break;
+			case "empty":
+				if(stack.isEmpty()) System.out.print(1);
+				else  System.out.print(0);
+				break;
+			}
 		}
 	}
 }
