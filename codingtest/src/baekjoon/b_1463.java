c49dd3cef6e77305215534f46a6f8cb984fd2b1f
YeoEunJi <yenje7@naver.com>
2020-04-24 10:51:07 +0900

    first commit

--- codingtest/src/baekjoon/b_1463.java
+++ codingtest/src/baekjoon/b_1463.java
@@ -1,5 +1,33 @@
 package baekjoon;
 
-public class b_1463 {
+import java.util.Scanner;
 
+public class b_1463 {
+	public static void main(String args[]) {
+		
+		Scanner s= new Scanner(System.in);
+		
+		int N = s.nextInt();
+		int cnt = 0;
+		int min = 100000000; 
+		
+		while(N==1) {
+			
+			if(N/3==0) {
+				N/=3;
+				
+			}
+			if(N/2==0) {
+				N/=2;
+				
+			}
+			if(N>1) {
+				N-=1;
+				
+			}
+			cnt++;
+			
+		}
+		
+	}
 }
--- codingtest/src/baekjoon/b_2156.java
+++ codingtest/src/baekjoon/b_2156.java
@@ -1,5 +1,18 @@
 package baekjoon;
 
-public class b_2156 {
+import java.util.Scanner;
 
+public class b_2156 {
+public static void main(String args[]) {
+		
+		Scanner s= new Scanner(System.in);
+		
+		int N = s.nextInt();
+		
+		int dp[] = new int[6];
+		
+		for(int i=0;i<N;i++) {
+			dp[i] = s.nextInt();
+		}
+	}
 }
