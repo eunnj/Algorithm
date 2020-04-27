32505ac4a846ea35801485b1e9b4136f67508104
YeoEunJi <yenje7@naver.com>
2020-04-27 21:39:25 +0900

    수정 필요

--- codingtest/src/baekjoon/b_1463.java
+++ codingtest/src/baekjoon/b_1463.java
@@ -13,17 +13,17 @@ public class b_1463 {
 		dp[0]=0; dp[1]=0; //√ ±‚»≠
 		
 		
+		/*Bottom-up*/
 		for(int i=2;i<=N;i++) {
-			
 			if(i/3==0 && i/2==0) {
-				dp[i] = Math.min(dp[i/3], Math.min(dp[i-1], dp[i-2]))+1;
+				dp[i] = Math.min(dp[i/3], Math.min(dp[i-1], dp[i-2])) + 1;
 			}
 			else if(i%3==0) {
-				dp[i]=Math.min(dp[i/3],dp[i-1])+1;
+				dp[i] = Math.min(dp[i/3],dp[i-1]) + 1;
 			}else if(i%2==0) {
-				dp[i]=Math.min(dp[i/2], dp[i-1])+1;
+				dp[i] = Math.min(dp[i/2], dp[i-1]) + 1;
 			}else {
-				dp[i]=dp[i-1]+1;
+				dp[i] = dp[i-1] + 1;
 			}
 		}
 		
--- codingtest/src/baekjoon/b_2156.java
+++ codingtest/src/baekjoon/b_2156.java
@@ -9,10 +9,25 @@ public static void main(String args[]) {
 		
 		int N = s.nextInt();
 		
-		int dp[] = new int[6];
+		int dp[] = new int[N];
+		int arr[] = new int[N];
 		
 		for(int i=0;i<N;i++) {
-			dp[i] = s.nextInt();
+			 arr[i] = s.nextInt();
 		}
+		
+		 
+		dp[0] = arr[0];
+		dp[1] = dp[0] + arr[1];
+
+
+		for(int i=3 ;i<N;i++) {
+			int max1 = Math.max(dp[i-2]+arr[i],dp[i-3]+arr[i-1]+arr[i]);
+			int max2 = Math.max(dp[i-1], dp[i]);
+			
+			dp[i] = Math.max(max1, max2);
+		}
+		
+		System.out.println(dp[N-1]);
 	}
 }
