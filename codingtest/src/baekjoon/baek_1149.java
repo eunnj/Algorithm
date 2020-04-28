c6938556334ff3242faf0b2b2bebadfcb9208273
YeoEunJi <yenje7@naver.com>
2020-04-28 17:02:20 +0900

    백준 1149번 RGB거리

--- codingtest/src/baekjoon/baek_1149.java
+++ codingtest/src/baekjoon/baek_1149.java
@@ -7,7 +7,6 @@ public class baek_1149 {
 		Scanner s = new Scanner(System.in);
 		
 		int N = s.nextInt(); //집의 수
-		//int graph[][] = new int[3][N];
 		int dp[][] = new int[3][N];
 		
 		for(int i=0;i<N;i++) { 
@@ -18,31 +17,18 @@ public class baek_1149 {
 		
 		
 		for(int i=1;i<N;i++) { //dp[0]은 초기화 상태로 같으므로 생략하고 1부터 시작
-			for(int j=0;j<3;j++) {
-				if(j==0) dp[i][j] += Math.min(dp[i-1][j+1], dp[i-1][j+2]);
-				else if(j==1) dp[i][j] += Math.min(dp[i-1][j+1], dp[i-1][j-1]);
-				else dp[i][j] += Math.min(dp[i-1][j-1], dp[i-1][j-2]);
-				
-			}
+			 dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]);
+			 dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]);
+			 dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]);
 		}
 		
-		int min = 1000;
+		int min = 100000;
 		
 		for(int i=0;i<3;i++) {
 			min = Math.min(min, dp[N-1][i]);
-			
 		}
-		/*
 		
-		for(int i=0;i<N;i++) { 
-			for(int j=0;j<3;j++) {//RGB
-				System.out.println(dp[i][j]);
-			}
-			System.out.println();
-		}
-	*/
 		System.out.println(min);
-		
-		
+
 	}
 }
