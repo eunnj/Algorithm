ae2498cf10edb219a82147fdb8bd92b46acb5d7a
YeoEunJi <yenje7@naver.com>
2020-04-28 15:49:49 +0900

    백준 2156번 포도주 시식

--- codingtest/src/baekjoon/b_2156.java
+++ codingtest/src/baekjoon/b_2156.java
@@ -9,25 +9,24 @@ public static void main(String args[]) {
 		
 		int N = s.nextInt();
 		
-		int dp[] = new int[N];
-		int arr[] = new int[N];
+		int dp[] = new int[N+1]; // 가장 많이 마실 수 있는 포도주 양
+		int arr[] = new int[N+1]; //포도주의 양
 		
-		for(int i=0;i<N;i++) {
+		for(int i=1;i<=N;i++) {
 			 arr[i] = s.nextInt();
 		}
 		
 		 
-		dp[0] = arr[0];
-		dp[1] = dp[0] + arr[1];
+		dp[1] = arr[1];
+		dp[2] = dp[1] + arr[2];
 
 
-		for(int i=3 ;i<N;i++) {
-			int max1 = Math.max(dp[i-2]+arr[i],dp[i-3]+arr[i-1]+arr[i]);
-			int max2 = Math.max(dp[i-1], dp[i]);
+		for(int i=3 ;i<=N;i++) {
+			dp[i] = Math.max(dp[i-2]+arr[i],dp[i-3]+arr[i-1]+arr[i]); //마지막 i잔을 마셨을 경우
+			dp[i] = Math.max(dp[i-1], dp[i]); //포도주를 두 번 연속 안먹었을 경우
 			
-			dp[i] = Math.max(max1, max2);
 		}
 		
-		System.out.println(dp[N-1]);
+		System.out.println(dp[N]);
 	}
 }
