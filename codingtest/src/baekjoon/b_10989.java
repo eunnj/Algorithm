73e3f564c89c0df4ca6cf19ba43f1db8192695be
YeoEunJi <yenje7@naver.com>
2020-05-01 19:37:37 +0900

    백준 10989번 수 정렬하기3

--- codingtest/src/baekjoon/b_10989.java
+++ codingtest/src/baekjoon/b_10989.java
@@ -1,63 +1,38 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class b_10989 {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-	
-		int N = s.nextInt();
-		int arr[]=new int[N];
+	public static void main(String[] args) throws Exception  {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); 
+		int arr[]=new int[10001];
 		
 		for(int i=0;i<N;i++) {
-			arr[i]=s.nextInt();
+			arr[Integer.parseInt(br.readLine())]++; //숫자 개수 count
 		}
 		
-		
-		/*버블정렬*/
-		/*	
-	 	for(int i=0;i<N;i++) {
-			for(int j=1;j<N;j++) {
-				if(arr[j-1]>arr[j]) {
-					int temp = arr[j-1];
-					arr[j-1]=arr[j];
-					arr[j]=temp;
-				}
-			}
-		}
-	 	*/
-		
-		
-		
-		quicksort(arr,0,N-1);
+		 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	        
+	        for(int i=0; i < arr.length; i++) {
+	            if(arr[i] > 0) { //숫자 개수가 있을 때
+	                for(int j = 0; j < arr[i]; j++) {
+	                    bw.write(Integer.toString(i) + "\n");
+	                }
+	            }
+	        }
+	        
+	        br.close();
+	        bw.close();
 
-			
-		for(int i=0;i<N;i++) {
-			System.out.println(arr[i]);
-		}	
-	}
-	
-	/*퀵 정렬*/
-	public static void quicksort(int[] arr,int left,int right) {
-		int l=left;
-		int r= right; 
-		int pivot = (l+right)/2;
-		
-		while(l<r) {
-			while(arr[pivot]>arr[l]) l++;
-			while(arr[pivot]<arr[r]) r--;
-			
-			if(l<=r) {
-				int temp=arr[r];
-				arr[r]=arr[l];
-				arr[l]=temp;
-				l++;
-				r--;
-			}
-		}
-		
-		if(left<r) quicksort(arr,left,r);
-		if(l<right) quicksort(arr,l,right);
 
+		
+		}
 	}
-}
+
