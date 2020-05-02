516ac733eb61965b30a612a295fce473ef78ef5a
YeoEunJi <yenje7@naver.com>
2020-05-02 13:59:24 +0900

    백준 1463번 1로 만들기

--- codingtest/src/baekjoon/b_1463.java
+++ codingtest/src/baekjoon/b_1463.java
@@ -25,7 +25,7 @@ public class b_1463 {
 		
 		/*Bottom-up*/
 		for(int i=2;i<=N;i++) {
-			if(i/3==0 && i/2==0) {
+			if(i%3==0 && i%2==0) {
 				dp[i] = Math.min(dp[i/3], Math.min(dp[i-1], dp[i-2])) + 1;
 			}
 			else if(i%3==0) {
