f6b5534e2eeb78647f5bf8dd5c5ae10122d87c1b
YeoEunJi <yenje7@naver.com>
2020-05-06 14:31:12 +0900

    백준 11726번 2×n 타일링

--- codingtest/src/baekjoon/bj_11726.java
+++ codingtest/src/baekjoon/bj_11726.java
@@ -1,5 +1,26 @@
 package baekjoon;
 
-public class bj_11726 {
+import java.util.Scanner;
 
+public class bj_11726 {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		
+		int N = s.nextInt();
+		
+		int dp[] = new int[N+1];
+		
+		dp[0]=0;
+		
+		if(N>=1) dp[1]=2;
+			
+			
+		for(int i=2;i<=N;i++) {
+			dp[i]= (dp[i-1]+dp[i-2]) % 10007;
+		}
+		
+		
+		System.out.println(dp[N]);
+		
+	}
 }
