8efaec05b3393833db7587ceab582a50f687f141
YeoEunJi <yenje7@naver.com>
2020-05-07 17:19:30 +0900

    백준 2293번 동전 1

--- codingtest/src/baekjoon/bj_2293.java
+++ codingtest/src/baekjoon/bj_2293.java
@@ -5,7 +5,23 @@ import java.util.Scanner;
 public class bj_2293 {
 public static void main(String[] args) {
 		
-		Scanner s= new Scanner(System.in);
+	Scanner s = new Scanner(System.in);
 	
+	int N = s.nextInt();
+	int K = s.nextInt();
+	
+	int dp[] = new int[K+1];
+	
+	dp[0]=1;
+	
+	for(int i = 0; i < N; i++) {
+		int coin = s.nextInt();
+		for(int j = 1; j < K+1; j++) {
+			if(j >= coin)
+				dp[j] += dp[j - coin];
+		}
+	}
+	System.out.println(dp[K]);
+		
 	}
 }
