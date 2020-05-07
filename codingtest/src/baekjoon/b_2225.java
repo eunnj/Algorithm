cc7846cd9f1b136a3efd1554020983c82b865736
YeoEunJi <yenje7@naver.com>
2020-05-07 17:20:18 +0900

    컴파일 에러

--- codingtest/src/baekjoon/b_2225.java
+++ codingtest/src/baekjoon/b_2225.java
@@ -9,18 +9,19 @@ public class b_2225 {
 		int N = s.nextInt();
 		int K = s.nextInt();
 		
-		int dp[] = new int[K+1];
+		int dp[][] = new int[N+1][K+1];
 		
-		dp[0]=1;
+		for(int i=0;i<=N;i++) {
+			dp[i][1]=1;
+		}
 		
-		dp[0] = 1;
-		for(int i = 0; i < N; i++) {
-			int coin = s.nextInt();
-			for(int j = 1; j < K+1; j++) {
-				if(j >= coin)
-					dp[j] += dp[j - coin];
+		
+		for(int i=1;i<=N;i++) {
+			for(int j=2;j<=K;j++) {
+				dp[i][j]=(dp[i-1][j]+dp[i][j-1])%1000000000;
 			}
 		}
-		System.out.println(dp[K]);
+
+		System.out.println(dp[N][K]);
 	}
 }
