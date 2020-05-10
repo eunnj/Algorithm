5fd06233458436106196a8e5bad2f8e8ce16e084
YeoEunJi <yenje7@naver.com>
2020-05-10 17:58:41 +0900

    백준 3036번 링

--- codingtest/src/baekjoon/bj_3036.java
+++ codingtest/src/baekjoon/bj_3036.java
@@ -1,5 +1,27 @@
 package baekjoon;
 
-public class bj_3036 {
+import java.util.Scanner;
 
+public class bj_3036 {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		
+		int N =s.nextInt(); //링의 개수
+		int first = s.nextInt(); //첫 번째 링
+		for(int i=0;i<N-1;i++) {
+			int rad = s.nextInt();
+			int gcd = gcd(first,rad);
+			System.out.print(first/gcd+"/"+rad/gcd);
+		}
+		
+		
+	}
+	public static int gcd(int a, int b){
+		while(b!=0){
+			int r = a%b;
+			a= b;
+			b= r;
+		}
+		return a;
+	}
 }
