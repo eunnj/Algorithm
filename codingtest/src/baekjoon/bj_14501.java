b2ef9ee53ccdc9d32865bca49b0a195090b547f9
YeoEunJi <yenje7@naver.com>
2020-05-17 22:39:25 +0900

    백준 14501번 퇴사

--- codingtest/src/baekjoon/bj_14501.java
+++ codingtest/src/baekjoon/bj_14501.java
@@ -6,9 +6,9 @@ public class bj_14501 {
 	public static void main(String[] args) {
 		Scanner s= new Scanner(System.in);
 		int N = s.nextInt();
-		int time[] = new int[N+1];
-		int pay[] = new int[N+1];
-		int dp[] = new int[N+2];
+		int time[] = new int[N+1]; //상담을 완료하는데 걸리는 기간
+		int pay[] = new int[N+1]; //상담을 했을 때 받을 수 있는 금액
+		int dp[] = new int[N+2]; //최대 이익
 	
 		for(int i=1;i<=N;i++) {
 			time[i]=s.nextInt();
