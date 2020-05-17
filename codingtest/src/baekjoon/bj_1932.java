b7b0976d1d0700438da887c1e76b335133ad7294
YeoEunJi <yenje7@naver.com>
2020-05-17 22:40:02 +0900

    백준 1932번 정수 삼각형

--- codingtest/src/baekjoon/bj_1932.java
+++ codingtest/src/baekjoon/bj_1932.java
@@ -6,8 +6,8 @@ public class bj_1932 {
 	public static void main(String[] args) {
 		Scanner s= new Scanner(System.in);
 		
-		int N = s.nextInt();
-		int dp[][] = new int[N+1][N+1];
+		int N = s.nextInt(); //삼각형 크기
+		int dp[][] = new int[N+1][N+1]; //각 자리 수
 		int max = 0;
 		
 		for(int i=1;i<=N;i++) {
@@ -18,11 +18,11 @@ public class bj_1932 {
 		
 		for(int i=1;i<=N;i++) {
 			for(int j=1;j<=i;j++) {
-				if(j==1)
+				if(j==1) //왼쪽 대각성 경로로 내려올 경우
 					dp[i][j]+=dp[i-1][1];
-				else if (j==i)
+				else if (j==i) //오른쪽 대각선 경로로 내려올 경우
 					dp[i][j]+=dp[i-1][j-1];
-				else
+				else //그 외 경우 왼쪽,오른쪽 대각선 값 중 더 큰값을 더해준다.
 					dp[i][j]+=Math.max(dp[i-1][j-1], dp[i-1][j]);
 			}
 		}
