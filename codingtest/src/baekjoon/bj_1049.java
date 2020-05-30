1d2606f9f57dfdc5ee0114ed559ca9d56f352fd0
YeoEunJi <yenje7@naver.com>
2020-05-30 13:10:41 +0900

    third

--- codingtest/src/baekjoon/bj_1049.java
+++ codingtest/src/baekjoon/bj_1049.java
@@ -8,7 +8,7 @@ public class bj_1049 {
 		
 		int N = s.nextInt();
 		int M = s.nextInt();
-		int min = 100000;
+		int Min = 100000;
 		
 		//int dp[][]=new int[M+1][N+1]; //최소 비용
 		int m[][]=new int[M+1][3]; //각 브랜드 패키지 가격과 낱개 가격
@@ -20,12 +20,12 @@ public class bj_1049 {
 			}
 		}
 		
-		int min6=100;
-		int min1=100;
+		int minP=100;
+		int minO=100;
 		
 		for(int j=1;j<=M;j++) {
-			min6=Math.min(min6,m[j][1]);
-			min1=Math.min(min6,m[j][2]);
+			minP=Math.min(minP,m[j][1]); //패키지 최소 금액
+			minO=Math.min(minO,m[j][2]); //낱개 최소 금액
 		}
 		
 //		//다른 브랜드의 기타줄도 가능.. 수정필요
@@ -39,14 +39,17 @@ public class bj_1049 {
 //		}
 //		
 		for(int i=1;i<=N;i++) {
-			if(i%6==0)
-				dp[i]=Math.min((i/6)*min6,dp[i-1]+min1);
-			else
-				dp[i]=Math.min(((i/6)+1)*min6,dp[i-1]+min1);
+				
+				dp[i]=Math.min(((i/6)+1)*minP,dp[i-1]+minO);
+				dp[i]=Math.min(dp[i],(i/6)*minP+(i%6)*minO);
 				
 		}
 		
 		
+//		//가장 싼 6개짜리 팩 구매 vs 가장 싼 낱개 구매 vs (가장싼 6개짜리 팩 + 낱개)
+//        Min = Math.min(((N/6)+1)*minP, N*minO);    
+//        Min = Math.min(Min, ((N/6))*minP + (N%6)*minO);
+        
 //		for(int i=1;i<=M;i++)
 //			min = Math.min(min,dp[i][N]);
 		
