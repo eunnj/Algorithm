0067f9750c28a65e201054b3788549e929a94ca3
yenje <yenje@LAPTOP-SIN1I3LK>
2020-06-02 06:26:00 +0900

    first

--- Algorithm/src/baekjoon/bj_1965.java
+++ Algorithm/src/baekjoon/bj_1965.java
@@ -8,13 +8,25 @@ public class bj_1965 {
 		
 		int N = s.nextInt();
 		
-		int size[] = new int[N+1];
-		int dp[] = new int[N+1];
+		int size[] = new int[N+2];
+		int dp[] = new int[N+2];
+		
+		int max=0;
 		
 		for(int i=1;i<=N;i++) {
 			size[i]=s.nextInt();
 		}
 		
+		for(int i=N;i>=1;i--) {
+			for(int j=N;j>=1;j--) {
+				if(size[j]>=size[j-1]) dp[i]++;
+				
+				if(dp[i]>dp[i+1]) max = dp[i];
+				else max = dp[i+1];
+			
+			}
+		}
 		
+		System.out.print(max);
 	}
 }
