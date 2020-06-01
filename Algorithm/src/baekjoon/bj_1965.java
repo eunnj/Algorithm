5c2a327ae2dc59823dc27ca036a950bb399b37eb
yenje <yenje@LAPTOP-SIN1I3LK>
2020-06-01 22:24:24 +0900

    init

---
+++ Algorithm/src/baekjoon/bj_1965.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class bj_1965 {
+	public static void main(String[] args ) {
+		Scanner s= new Scanner(System.in);
+		
+		int N = s.nextInt();
+		
+		int size[] = new int[N+1];
+		int dp[] = new int[N+1];
+		
+		for(int i=1;i<=N;i++) {
+			size[i]=s.nextInt();
+		}
+		
+		
+	}
+}
