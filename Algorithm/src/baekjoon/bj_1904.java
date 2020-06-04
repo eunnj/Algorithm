767812bf51fbbb867bdce6fb749591c9faa8b413
eunnj <yenje7@naver.com>
2020-06-05 08:24:23 +0900

    벡준 1904번 01타일

--- Algorithm/src/baekjoon/bj_1904.java
+++ Algorithm/src/baekjoon/bj_1904.java
@@ -1,5 +1,22 @@
 package baekjoon;
 
-public class bj_1904 {
+import java.util.Scanner;
 
+public class bj_1904 {
+	public static void main(String[] args ) {
+		
+		Scanner s= new Scanner(System.in);
+		
+		int N = s.nextInt();
+		int dp[] = new int[N+1];
+		
+		dp[1]=1;
+		dp[2]=2;
+		
+		for(int i=3;i<=N;i++) {
+			dp[i]=(dp[i-1]+dp[i-2])% 15746;
+		}
+		
+		System.out.println(dp[N]);
+	}
 }
