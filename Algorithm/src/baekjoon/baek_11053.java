ff22456da0dd88b883e7594224d9ac4b35679f3d
eunnj <yenje7@naver.com>
2020-06-08 15:52:59 +0900

    second

--- Algorithm/src/baekjoon/baek_11053.java
+++ Algorithm/src/baekjoon/baek_11053.java
@@ -7,35 +7,29 @@ public class baek_11053 {
 		Scanner s = new Scanner(System.in);
 		
 		int N = s.nextInt();
-		int arr[] = new int[N];
-		int dp[] = new int[N];
+		int arr[] = new int[N+1];
+		int dp[] = new int[N+1];
 		
-		for(int i=0;i<N;i++) {
+		for(int i=1;i<=N;i++) {
 			arr[i] = s.nextInt();
 		}
 		
-		for(int i=0; i<N; i++) {
-			dp[i]=1;
+		dp[1]=1;
+		
+
+		for(int i=2;i<=N;i++) {
+			if(arr[i-1]<arr[i]) {
+				dp[i]=(dp[i-1]+1);
+			}
+			else dp[i]=dp[i-1];
 		}
 		
-		int max = 0;
 		
-		for(int i=1;i<N;i++) { //초기값은 arr[0]이기 때문에 생략
-			for(int j=0;j<i;j++) {
-				if(j==0) {
-					if(arr[i]>arr[j]) dp[i]+=1;
-				}
-				else if(j!=0 && arr[i]>arr[j] && arr[j]>arr[0] && arr[j]>arr[j-1]) dp[i]+=1;
-			
-			 max = Math.max(max, dp[i]);
-		}
-	}
-		System.out.println(max);
-		
-		/*for(int i=0; i<N; i++) {
+		for(int i=1; i<=N; i++) {
 			System.out.println(dp[i]);
 
-		}*/
-		
+		}
+	
+		//System.out.println(dp[N]);
 	}
 }
