29b402d96d2b4a47ef60cd2f914b78814905e340
eunnj <yenje7@naver.com>
2020-06-24 07:10:31 +0900

    first init

---
+++ Algorithm/src/baekjoon/bj_11066.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class bj_11066 {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		
+		int T = s.nextInt();
+		
+		for(int i=0;i<T;i++) {
+			int K = s.nextInt();
+			for(int j=1;j<=K;j++) {
+				int dp[]= new int[K+1];
+				//점화식
+				
+				//결과 출력
+			}
+		}
+	}
+}
