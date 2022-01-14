9dc26b016a8328f8ec1e2040250fa9342c6ed90f
user <yenje7@naver.com>
2022-01-14 13:05:55 +0900

    프로그래머스 두 개 뽑아서 더하기

---
+++ programmers/src/Level1/Taketwo_andAdd.java
@@ -0,0 +1,30 @@
+package Level1;
+import java.util.*;
+
+public class Taketwo_andAdd {
+	class Solution {
+	    public int[] solution(int[] numbers) {
+	        int[] answer = {};
+	        ArrayList<Integer> list = new ArrayList<Integer>();
+	        
+	        for(int i=0;i<numbers.length;i++){
+	            for(int j=i+1;j<numbers.length;j++){
+	                int num=numbers[i]+numbers[j];
+	                if(!list.contains(num)) {
+	                    list.add(num);}
+	            }
+	        }
+	        
+	        answer = new int[list.size()];
+	        
+	        for(int i=0;i<answer.length; i++){
+	            answer[i] = list.get(i);
+	        }
+	    
+	        
+	        Arrays.sort(answer);
+	        
+	        return answer;
+	    }
+	}
+}
