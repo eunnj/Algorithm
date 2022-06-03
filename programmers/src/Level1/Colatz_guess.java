0dfb6866fc3a94198c8b2f9b64d65ed3736a7e79
user <yenje7@naver.com>
2022-06-04 02:19:46 +0900

    Lv1 콜라츠의 추측

--- programmers/src/Level1/Colatz_guess.java
+++ programmers/src/Level1/Colatz_guess.java
@@ -5,9 +5,9 @@ public class Colatz_guess {
 	    public int solution(long num) {
 	        int answer = 0;
 	        
-	        while(num!=1){
-	            if(num%2==0) num/=2;
-	            else num=num*3+1;
+	        while(num != 1){
+	            if(num %2 ==0) num/=2;
+	            else num = num*3 + 1;
 	            
 	            answer++;
 	            
