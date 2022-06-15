824110ce56b9e27157b51066f35096934a08c149
user <yenje7@naver.com>
2022-06-15 10:55:55 +0900

    setting

---
+++ Algorithm/src/baekjoon/bj_14888.java
@@ -0,0 +1,23 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class bj_14888 {
+	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		
+		int T = s.nextInt();
+		
+		int arr[] = new int[T];
+		int op[] = new int[4];
+		
+		for(int i=0;i<T;i++) {
+			arr[i] = s.nextInt();
+		}
+		
+		for(int i=0;i<op.length;i++) {
+			op[i] = s.nextInt();
+		}
+		
+		
+	}
+}
