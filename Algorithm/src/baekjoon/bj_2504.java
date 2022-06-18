f9b4169cc0ca1413822cf668e492287b69697270
user <yenje7@naver.com>
2022-06-18 15:49:58 +0900

    괄호의 값

--- Algorithm/src/baekjoon/bj_2504.java
+++ Algorithm/src/baekjoon/bj_2504.java
@@ -1,7 +1,50 @@
 package baekjoon;
+import java.util.Scanner;
+import java.util.Stack;
 
 public class bj_2504 {
 	public static void main(String[] args) {
+		Scanner s= new Scanner(System.in);
+		Stack<Character> stack = new Stack<>();
+		int ret=1;
+		int total =0;
+		String str = s.next();
 		
+		for(int i=0;i<str.length();i++) {
+			char ch = str.charAt(i);
+			
+			switch(ch) {
+			case '(':
+				stack.push(ch);
+				ret*=2;
+				break;
+			case '[':
+				stack.push(ch);
+				ret*=3;
+				break;
+			case ')':
+				if(stack.isEmpty()||stack.peek()!='(') {
+					System.out.print(0);
+					return;
+				} else if (str.charAt(i - 1) == '(') {
+                    total += ret;
+                }
+                stack.pop();
+                ret /= 2;
+                break;
+			case ']':
+				if(stack.isEmpty()||stack.peek()!='[') {
+					System.out.print(0);
+					return;
+				} else if (str.charAt(i - 1) == '[') {
+                    total += ret;
+                }
+                stack.pop();
+                ret /= 3;
+                break;
+			}
+			
+		}
+		System.out.print(total);
 	}
 }
