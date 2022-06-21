3d121cfb690e1414192e7050e14ee1732c03a413
user <yenje7@naver.com>
2022-06-21 22:28:00 +0900

    스택

--- codingtest/src/baekjoon/b_10828.java
+++ codingtest/src/baekjoon/b_10828.java
@@ -1,42 +1,45 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
 
 public class b_10828 {
 
-	public static void main(String[] args) {
-		//https://www.acmicpc.net/problem/10828
+	public static void main(String[] args) throws IOException{
 		Scanner s= new Scanner(System.in);
-		Stack<Integer> stack = new Stack<Integer>();
-		
-		int N= s.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack();
+		int N = Integer.parseInt(br.readLine());
 		
 		for(int i=0;i<N;i++) {
-			String order=s.next();
+			String[] str = br.readLine().split(" ");
 			
-			switch(order) {
-			case "push":
-				int x = s.nextInt();
-				 stack.push(x);
+			switch(str[0]) {
+			case "push" :
+				int x = Integer.parseInt(str[1]);
+				stack.push(x);
 				break;
 			case "pop":
-				if(stack.isEmpty()) System.out.println(-1);
-				else  System.out.println(stack.pop());
+				if(stack.isEmpty()) System.out.println("-1");
+				else System.out.println(stack.pop());
 				break;
 			case "size":
-				 System.out.println(stack.size());
-				break;
-			case "top":
-				if(stack.isEmpty()) System.out.println(-1);
-				else  System.out.println(stack.peek());
+				System.out.println(stack.size());
 				break;
 			case "empty":
-				if(stack.isEmpty()) System.out.println(1);
-				else  System.out.println(0);
+				if(stack.isEmpty()) System.out.println("1");
+				else System.out.println("0");
+				break;
+			case "top":
+				if(stack.isEmpty()) System.out.println("-1");
+				else System.out.println(stack.peek());
 				break;
 			}
 		}
+		
 	}
 }
