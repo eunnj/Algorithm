2947288a7597ea417f80b92e70af67aec7613c7c
eunnj <43837984+eunnj@users.noreply.github.com>
2023-01-18 23:00:15 +0900

    [level 2] Title: 피보나치 수, Time: 2.31 ms, Memory: 69.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int fib[] = new int[n+1];
+        int f[] = new int[n+1];
         
-        fib[0]=0;
-        fib[1]=1;
+        f[0]=0;
+        f[1]=1;
         
         for(int i=2;i<=n;i++){
-            fib[i]=((fib[i-2])%1234567+(fib[i-1])%1234567)%1234567;
+            f[i]=(f[i-2]+f[i-1])%1234567;
         }
-        
-        answer=fib[n];
-        
+       
+        answer = f[n]%1234567;
         return answer;
     }
+    
 }
