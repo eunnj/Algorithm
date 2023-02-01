c9a5dc447979a068bcfcaa3bb2feca6b16289690
eunnj <43837984+eunnj@users.noreply.github.com>
2023-02-01 22:47:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0;i<absolutes.length;i++){
+            if(signs[i]){
+                answer+=absolutes[i];
+            }else{
+                answer+=(absolutes[i]*(-1));
+            }
+            
+        }
+        
+        return answer;
+    }
+}
