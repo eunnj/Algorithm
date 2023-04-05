20edda5e65695a1891d36799524359139b9fd739
eunnj <43837984+eunnj@users.noreply.github.com>
2023-04-05 23:08:27 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME LIKE 'Lucy' OR
+      NAME LIKE 'Ella' OR
+      NAME LIKE 'Pickle' OR
+      NAME LIKE 'Rogan' OR
+      NAME LIKE 'Sabrina' OR
+      NAME LIKE 'Mitty'
+ORDER BY ANIMAL_ID;
