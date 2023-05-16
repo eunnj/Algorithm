5bc54e6e0629fa5e94487d658f902a908526f2fb
eunnj <43837984+eunnj@users.noreply.github.com>
2023-05-17 00:42:50 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner s= new Scanner(System.in);
+		
+		int N = s.nextInt();
+		int dp[] = new int[1001]; // (1 ≤ n ≤ 1,000)
+		
+		dp[1]=1;
+		dp[2]=3;
+		
+		for(int i=3;i<=N;i++) {
+			dp[i]=(dp[i-1]+2*dp[i-2])%10007;
+			
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
