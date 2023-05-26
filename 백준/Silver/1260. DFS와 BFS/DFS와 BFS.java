5c96d5bfc1c4d3200cf0e9f754099c881f52706c
eunnj <43837984+eunnj@users.noreply.github.com>
2023-05-27 02:15:25 +0900

    [Silver II] Title: DFS와 BFS, Time: 324 ms, Memory: 26160 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,96 +1,90 @@
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
-	static boolean[] check;
-	static ArrayList<Integer> nodes[];
-	static int N;
-	public static void main(String[] args) {
-		Scanner s= new Scanner(System.in);
-	
-		int N = s.nextInt(); //정점의 개수
-		int M = s.nextInt(); //간선의 개수
-		int V = s.nextInt(); //정점의 번호
+	public static String str = "";
+	public static void main(String[] args) throws IOException{
 		
-		check =new boolean[N+1];
-		nodes = new ArrayList[N+1];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		for(int i=1; i<=N; i++) {
-			nodes[i] = new ArrayList();
-			check[i]=false;
+	    String str_arr[] = br.readLine().split(" ");
+		
+		int N = Integer.parseInt(str_arr[0]);
+		int M = Integer.parseInt(str_arr[1]);
+		int V = Integer.parseInt(str_arr[2]);
+		LinkedList<Integer> list[] = new LinkedList[N+1];
+		boolean visited[] = new boolean[N+1]; // 방문 여부 
+		
+		for (int i = 0; i <= N; i++) {
+			list[i] = new LinkedList<Integer>();
+		}
+
+		
+		for(int i=0;i<M;i++) {
+			String str[] = br.readLine().split(" ");
+			int v1 =  Integer.parseInt(str[0]);
+			int v2 =  Integer.parseInt(str[1]);
+			
+			list[v1].add(v2);
+			list[v2].add(v1);
 		}
 		
-		for (int i = 0; i < M; i++) {
-			int n1 = s.nextInt();
-			int n2 = s.nextInt();	
-			nodes[n1].add(n2); 
-			nodes[n2].add(n1);
-			Collections.sort(nodes[n1]);
-			Collections.sort(nodes[n2]);
-			
-	}
-		
-
-		dfs(V);
-		System.out.println();
-		
-		//초기화
-		for(int i=1; i<=N; i++) {
-			check[i]=false;
+		// 방문 순서 정렬 
+		for(int i=0;i<=N;i++) {
+			Collections.sort(list[i]);
 		}
 		
-		bfs(V);
-
+		dfs(V,list,visited);
+		
+		System.out.println(str);
+		
+		// 방문 초기화 
+		for (int i = 0; i <= N; i++) {
+			visited[i] = false;
+		}
+		
+		str=V+ " ";
+		
+		bfs(V,list,visited);
+		System.out.println(str);
 	}
 	
-	public static void dfs(int v) {
-		Stack<Integer> stack=new Stack<Integer>();
-		stack.push(v);
+	
+	
+	public static void dfs(int v,LinkedList<Integer> list[], boolean visited[]) {
+		str += v +" ";
+		visited[v]=true; // 방문 
+		Iterator<Integer> iter = list[v].listIterator();
 		
-		while(!stack.isEmpty()) {
-			int current = stack.pop();
-			
-			if(check[current]==false) {
-				check[current]=true;
-				System.out.print(current+" ");
-				
-				
-				for(int i:nodes[current]) {
-					if (check[i]==false)
-						dfs(i);
-				}
+		while(iter.hasNext()) {
+			int x = iter.next();
+			if(!visited[x]) { // 방문하지 않았을 때 
+				dfs(x,list,visited);
 			}
-			
 		}
-		
 	}
 	
-	public static void bfs(int v) {
-		Queue<Integer> que = new LinkedList();
-		que.add(v); //시작점
-		check[v] = true;
+	public static void bfs(int v,LinkedList<Integer> list[], boolean visited[]) {
+		Queue<Integer> queue = new LinkedList<Integer>();
 		
-		while(!que.isEmpty()) {
-			int current = que.poll();
-			
-			System.out.print(current+" ");
+		queue.add(v);
+		visited[v]=true; 
 		
-			  for(int i : nodes[current]){
-	                if(check[i]==false){
-	                    que.add(i);
-	                    check[i] = true;
-	                }
-	                
-	            }
-			
 
-		      
-		}
-		
+        while(!queue.isEmpty()){ 
+            int cur = queue.poll(); 
+            for(int next : list[cur]) {
+                if(!visited[next]) { 
+                    visited[next] = true; 
+                    queue.add(next);
+                    str+=next+ " ";
+                }
+            }
+        }
 	}
 }
