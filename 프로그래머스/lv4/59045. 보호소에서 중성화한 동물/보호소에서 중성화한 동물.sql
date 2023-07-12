791ef3e5af4deb131db4e978d652810002ac430d
YEO EUN JI <43837984+eunnj@users.noreply.github.com>
2023-07-12 22:52:28 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,7 +1,9 @@
 -- 코드를 입력하세요
-SELECT i.animal_id, i.animal_type, o.name
-from animal_ins i , animal_outs o
+SELECT a.animal_id,a.animal_type,a.name
+from animal_ins a, animal_outs b
 where 1=1
-    and i.animal_id = o.animal_id
-    and i.SEX_UPON_INTAKE != o.SEX_UPON_OUTCOME
+    and a.animal_id = b.animal_id
+    and a.SEX_UPON_INTAKE like '%Intact%'
+    and (b.SEX_UPON_OUTCOME like '%Spayed%' or b.SEX_UPON_OUTCOME like '%Neutered%')
+order by 1
 ;
