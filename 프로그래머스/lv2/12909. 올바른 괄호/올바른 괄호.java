754f46102aa8c4e66fda826c96233084c7edfa0c
YEO EUN JI <43837984+eunnj@users.noreply.github.com>
2023-07-12 22:30:38 +0900

    [level 2] Title: 올바른 괄호, Time: 18.04 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,20 +1,19 @@
 import java.util.Stack;
 class Solution {
     boolean solution(String s) {
-        boolean answer = false;
+        boolean answer = true;
+
         Stack<Character> stack = new Stack();
         
         for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(ch=='('){
-                stack.add(ch);
-            }else{
-                if(stack.isEmpty()) return answer;
-                else stack.pop();
+            if(s.charAt(i)=='(') stack.add('(');
+            else {
+                if(!stack.isEmpty()) stack.pop();
+                else return false;
             }
         }
 
-        answer =  stack.isEmpty() ? true:false;
+        if(!stack.isEmpty()) answer = false;
         return answer;
     }
 }
