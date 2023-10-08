dfd0f82c74d5537a5b1abf1408c884caf77f9b90
YEO EUN JI <43837984+eunnj@users.noreply.github.com>
2023-10-08 22:44:12 +0900

    Time: 0 ms (100.00%), Space: 38.8 MB (95.97%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numTilings(int n) {
+        long[] dp = new long[n + 3]; 
+        dp[0] = 1; 
+        dp[1] = 2; 
+        dp[2] = 5;
+        for (int i = 3; i < n; i ++) {
+            dp[i] = (dp[i - 1] * 2 + dp[i - 3]) % 1000000007;
+        }
+        return (int)dp[n - 1];
+    }
+}
